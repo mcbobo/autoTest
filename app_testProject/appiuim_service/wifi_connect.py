dbac60b854fbc8e0ec5cc6f9f3c195d1a705f0e9
mcbobo <jgb742413096@gmail.com>
2018-10-18 22:51:13 +0800

    up

--- app_testProject/appiuim_service/wifi_connect.py
+++ app_testProject/appiuim_service/wifi_connect.py
@@ -15,22 +15,26 @@ class WifiConnect:
     adb无线连接多台Android设备
     连接一次设备后，保存设备ip到本地
     """
+    lock = multiprocessing.Lock()
 
     def __init__(self):
-        self.loc_pool = readInfo(PATH("../data/ip.pickle"))
+        self.ip_data = readInfo(PATH("../data/ip.pickle"))
         self.ipPort_pool = []
-        self.run_pool = []
         # self.lock = multiprocessing.Lock()
 
     # 多线程无线连接设备
     def connect_syc(self):
         ip_port_pool = self._ip_port()
         num = len(ip_port_pool) if len(ip_port_pool) < 5 else 5
-        pool = multiprocessing.Pool(processes=num)
-        pool.map(self.connect, ip_port_pool)
-        print(self.loc_pool)
-        print('start saving')
-        self.save_ip(self.loc_pool)
+
+        if num > 0:
+            pool = multiprocessing.Pool(processes=num)
+            pool.map(self.connect, ip_port_pool)
+            print(self.ip_data)
+            print("start saving")
+            self.save_ip(self.ip_data)
+        else:
+            print("没有可用的安卓设备")
 
     # adb无线连接设备
     def connect(self, ip_port):
@@ -49,7 +53,7 @@ class WifiConnect:
             # self.lock.acquire()
             print("unable to connect to %s" % ip_port)
             self._remove(ip)
-            print(self.loc_pool)
+            print(self.ip_data)
             # self.lock.release()
 
     def disconnect(self, ip_port):
@@ -65,11 +69,12 @@ class WifiConnect:
         write(data, PATH("../data/ip.pickle"))
 
     def _remove(self, data):
-        self.loc_pool.remove(data)
+        self.ip_data.remove(data)
 
     def _ip_port(self):
+        ip_pool = self._ip()
         port = 5555
-        for i in self.run_pool:
+        for i in ip_pool:
             ip_port = i + ':' + str(port)
             self.ipPort_pool.append(ip_port)
             port += 2
@@ -78,28 +83,33 @@ class WifiConnect:
     def _ip(self):
         """
         获取设备ip地址
-        :return: loc_pool
+        :return: run_pool
         """
         ip_pool = []
         cmd = 'adb devices'
         ip_str = 'inet addr:'
         dev = os.popen(cmd).readlines()
         len_devices = len(dev) - 1
-        for i in range(1, len_devices):
-            ip_cmd = 'adb -s ' + dev[i].split('\t')[0] + ' shell ifconfig wlan0'
-            ip_info = subprocess.Popen(ip_cmd, shell=True, stdout=subprocess.PIPE,
-                                       stderr=subprocess.PIPE).stdout.readlines()
-            for line in ip_info:
-                temp = line.decode().strip()
-                if temp.find(ip_str) >= 0:
-                    ip = temp[len(ip_str):].split()[0]
-                    if ip not in self.loc_pool:
-                        self.loc_pool.append(ip)
+
+        if len_devices > 1:
+            for i in range(1, len_devices):
+                ip_cmd = 'adb -s ' + dev[i].split('\t')[0] + ' shell ifconfig wlan0'
+                ip_info = subprocess.Popen(ip_cmd, shell=True, stdout=subprocess.PIPE,
+                                           stderr=subprocess.PIPE).stdout.readlines()
+                for line in ip_info:
+                    temp = line.decode().strip()
+                    if temp.find(ip_str) >= 0:
+                        ip = temp[len(ip_str):].split()[0]
                         ip_pool.append(ip)
-        self.run_pool = list(filter(lambda a: a not in self.loc_pool, ip_pool))
-        print(self.run_pool)
+                        if ip not in self.ip_data:
+                            self.ip_data.append(ip)
+            run_pool = list(filter(lambda x: x not in ip_pool, self.ip_data))
+            return run_pool
+        else:
+            run_pool = self.ip_data
+            return run_pool
 
 
 if __name__ == '__main__':
-    WifiConnect().connect_syc()
-    # WifiConnect()._ip()
+    # WifiConnect().connect_syc()
+    WifiConnect()._ip()
